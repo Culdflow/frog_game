06a60da964130f234d8b43de90ac9fb8c6eaa7ad
Feve Dimitri <fevedimitri@gmail.com>
2026-02-09 03:01:08 +0100

    winds

--- States/teleportingState.gd
+++ States/teleportingState.gd
@@ -2,6 +2,7 @@ extends State
 class_name TeleportingState
 
 @export var tp_speed = 100
+var kept_vel = Vector2(0, 0)
 
 func _enter():
 	print("[ENTER]: teleporting")
@@ -10,11 +11,12 @@ func _enter():
 		tp_tween.finished.connect(_tween_finished)
 		var distance = player.global_position.distance_to(player.boom.global_position)
 		tp_tween.tween_property(player, "global_position", player.boom.global_position, (distance / tp_speed))
+		kept_vel = player.boom.velocity
 		player.boom.queue_free()
 		player.boom = null
 
 func _tween_finished():
-	player.velocity.x = 0
+	player.velocity = kept_vel
 	Transitioned.emit(self, "idlestate")
 	player.isThrowing = false
 
