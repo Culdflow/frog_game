91a808b99c3b1caa6ad48f84ab63a8e3ab39bf2b
Feve Dimitri <fevedimitri@gmail.com>
2026-02-13 04:21:34 +0100

    added background wall and shaders post processing

--- test_scenes/test_scene.gd
+++ test_scenes/test_scene.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @export var mesh: MeshInstance3D
 @export var light: SpotLight3D
+@export var camera2d: Camera2D
 @export var camera: Camera3D
 @export var player: Frog
 @export var back_sprite: Sprite2D
@@ -10,7 +11,7 @@ var can_run: bool = false
 
 func _process(delta: float) -> void:
 	if can_run:
-		var player_y = player.position.y
+		var player_y = camera2d.global_position.y
 		var mat = mesh.get_active_material(0)
 		mat.uv1_offset.y = player_y * 0.0005
 		var player_x = player.global_position.x - back_sprite.global_position.x
